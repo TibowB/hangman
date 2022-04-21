cfad9b9afe7d1c8bd1b5cbdac0d58abcf1e8e390
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-21 22:29:42 +0200

    Init angular app

---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+modules.exports = {
+    tabWidth: 2
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
