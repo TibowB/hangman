e1b60158c56644048af5a138ca82bb8a430ccdda
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-22 19:09:59 +0200

    Add redirection to hangman view

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   onClickStartGameOnePlayer() {
-    console.log('One');
+    this.router.navigate(['hangman']);
   }
 
   onClickStartGameTwoPlayer() {
