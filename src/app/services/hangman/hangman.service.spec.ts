4c2f48d5c7def2b468505a938c3c4c037667e259
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-24 22:13:57 +0200

    Add tests for hangman service

--- src/app/services/hangman/hangman.service.spec.ts
+++ src/app/services/hangman/hangman.service.spec.ts
@@ -13,7 +13,70 @@ describe('HangmanService', () => {
     service = TestBed.inject(HangmanService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should insanciate default game object', () => {
+    expect(service.game.value).toEqual({
+      word: [],
+      hiddenWord: [],
+      tries: 0,
+      isWordFound: false,
+    });
+  });
+
+  it('should return false from isGameOver() if tries is not equal to 11, otherwise it returns true', () => {
+    // Tries = 0
+    expect(service.isGameOver()).toBeFalse();
+
+    // Tries = 10
+    service.game.next({
+      word: [],
+      hiddenWord: [],
+      tries: 10,
+      isWordFound: false,
+    });
+    expect(service.isGameOver()).toBeFalse();
+
+    // Tries = 11
+    service.game.next({
+      word: [],
+      hiddenWord: [],
+      tries: 11,
+      isWordFound: false,
+    });
+    expect(service.isGameOver()).toBeTrue();
+  });
+
+  it('should return true from hiddenWordIsFound() if word and hiddenWord are equals, otherwise it returns false', () => {
+    // Word !== HiddenWord
+    service.game.next({
+      word: ['F', 'O', 'O'],
+      hiddenWord: ['_', '_', '_'],
+      tries: 0,
+      isWordFound: false,
+    });
+    expect(service.hiddenWordIsFound()).toBeFalse();
+
+    // Word === HiddenWord
+    service.game.next({
+      word: ['F', 'O', 'O'],
+      hiddenWord: ['F', 'O', 'O'],
+      tries: 0,
+      isWordFound: true,
+    });
+    expect(service.hiddenWordIsFound()).toBeTrue();
+  });
+
+  it('should return true and update the hiddenWord if a submited letter is the word, otherwise it returns false', () => {
+    service.game.next({
+      word: ['F', 'O', 'O'],
+      hiddenWord: ['_', '_', '_'],
+      tries: 0,
+      isWordFound: true,
+    });
+
+    expect(service.isLetterInWord('O')).toBeTrue();
+    expect(service.game.value.hiddenWord).toEqual(['_', 'O', 'O']);
+
+    expect(service.isLetterInWord('B')).toBeFalse();
+    expect(service.game.value.hiddenWord).toEqual(['_', 'O', 'O']);
   });
 });
