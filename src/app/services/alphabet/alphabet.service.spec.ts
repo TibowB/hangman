1b87fbf576eb5ab81a8699da6fe64730522d341a
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-24 18:55:03 +0200

    Add test for AlphabetService

--- src/app/services/alphabet/alphabet.service.spec.ts
+++ src/app/services/alphabet/alphabet.service.spec.ts
@@ -10,7 +10,39 @@ describe('AlphabetService', () => {
     service = TestBed.inject(AlphabetService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should have a variable "alphabet" containing the alphabet', () => {
+    expect(service.alphabet).toEqual('abcdefghijklmnopqrstuvwxyz');
+  });
+
+  it('getAlphabet() returns an array contaning the alphabet in uppercase', () => {
+    const arrayAlphabet = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'W',
+      'X',
+      'Y',
+      'Z',
+    ];
+    expect(service.getAlphabet()).toEqual(arrayAlphabet);
   });
 });
