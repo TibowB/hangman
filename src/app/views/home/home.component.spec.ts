130d4dc177294383a763d984d4d1c3b9375d0654
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-25 14:11:04 +0200

    Add tests form home component

--- src/app/views/home/home.component.spec.ts
+++ src/app/views/home/home.component.spec.ts
@@ -1,17 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ButtonComponent } from 'src/app/components/button/button.component';
+import { routes } from '../../app-routing.module';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { HangmanService } from '../../services/hangman/hangman.service';
 
 describe('HomeComponent', () => {
+  let router: Router;
+  let location: Location;
   let component: HomeComponent;
+  let hangmanService: HangmanService;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, RouterTestingModule],
+      providers: [Location],
+      imports: [HttpClientModule, RouterTestingModule.withRoutes(routes)],
       declarations: [HomeComponent, ButtonComponent],
     }).compileComponents();
   });
@@ -20,9 +28,42 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    hangmanService = TestBed.inject(HangmanService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should call the method onClickStartGameOnePlayer() when we click on the button "OnePlayer"', fakeAsync(() => {
+    spyOn(component, 'onClickStartGameOnePlayer');
+
+    fixture.detectChanges();
+    const onePlayerButton = fixture.debugElement.nativeElement.querySelector(
+      'app-button:first-of-type'
+    );
+
+    onePlayerButton.click();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(component.onClickStartGameOnePlayer).toHaveBeenCalled();
+    });
+  }));
+
+  it('should call the method onClickStartGameTwoPlayer() when we click on the button "TwoPlayer"', fakeAsync(() => {
+    spyOn(component, 'onClickStartGameTwoPlayer');
+
+    fixture.detectChanges();
+
+    const twoPlayerButton = fixture.debugElement.nativeElement.querySelector(
+      'app-button:last-of-type'
+    );
+
+    twoPlayerButton.click();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(component.onClickStartGameTwoPlayer).toHaveBeenCalled();
+    });
+  }));
 });
